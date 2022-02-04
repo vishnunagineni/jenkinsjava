9b3de7dc3700f3feb61d7b0d8c3e6334c7b25df8
vishnunagineni <vishnu.nagineni@zensar.com>
2022-02-04 12:15:46 +0530

    first commit

---
+++ src/main/java/com/zensar/sample_mavenn/App.java
@@ -0,0 +1,13 @@
+package com.zensar.sample_mavenn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! By Jenkins" );
+    }
+}
